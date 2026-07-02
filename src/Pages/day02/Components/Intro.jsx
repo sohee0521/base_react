1208245e1798dc0dc38b4e29191b58fe19fec134
sohee0521 <qkrthgml0417@email.com>
2026-07-02 11:32:56 +0900

    수정

--- src/Pages/day02/Components/Intro.jsx
+++ src/Pages/day02/Components/Intro.jsx
@@ -3,10 +3,15 @@ export default function Intro(props) {
   //   const age = props.age;
 
   const { name, age, hobby } = props;
+  //   객체 비구조화 할당
 
   return (
     <div>
-      <div>
+      <div
+        style={{
+          background,
+        }}
+      >
         <h1>자기소개</h1>
         <p>이름:{name}</p>
         <p>나이: {age}</p>
